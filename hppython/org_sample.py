8c0d0092067f8eb98bc129ef1aaff05a63e02cad
msjung <jms7446@gmail.com>
2020-09-07 16:16:10 +0900

    check point

--- hppython/org_sample.py
+++ hppython/org_sample.py
@@ -110,3 +110,11 @@ if __name__ == "__main__":
     # reasonable defaults for a laptop
     # set draw_output to True to use PIL to draw an image
     calc_pure_python(draw_output=True, desired_width=1000, max_iterations=300)
+
+
+from util import *
+
+
+def test1():
+    timeit(calc_pure_python(draw_output=True, desired_width=1000, max_iterations=300)
+
